1f52a5cd75486569b95f72ba55ec1aace2a23277
senasa-1 <testsenasa5@gmail.com>
2025-02-12 16:13:03 -0300

    Add files via upload

---
+++ qr_files/script.js
@@ -0,0 +1,11 @@
+function generateQRCode() {
+	const url = 'https://facuarroba.github.io/senasa.test/index.html?';
+	const c = 'c=' + document.getElementById('txt_cuve').value;
+	const n = '&n=' + document.getElementById('txt_certificado').value;
+	const p = '&p=' + document.getElementById('txt_ptr').value;
+	const f = '&f=' + document.getElementById('txt_fecha').value;
+    const text = url + c + n + p + f;
+    const qrcodeContainer = document.getElementById('qrcode');
+    qrcodeContainer.innerHTML = ''; // Clear previous QR code
+    new QRCode(qrcodeContainer, text);
+}
